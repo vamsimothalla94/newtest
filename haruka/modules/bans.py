28681f1832b2c84ab9a4da9f6f33e597d7b4f8e5
Vamsimothalla <42707756+Vamsimothalla@users.noreply.github.com>
2020-06-21 17:54:42 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -44,7 +44,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     repl_message = message.reply_to_message
     my_id = repl_message.from_user.id
     mmy_id = update.effective_message.from_user.id
-    if not can_rest(chat,mmy_id):
+    if can_rest(chat,mmy_id):
         message.reply_text("invalid permission{}{}".format(my_id,mmy_id))
         return ""
     if user_id == bot.id:
