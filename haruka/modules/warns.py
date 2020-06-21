5e47007fdb557945d2357ecc0d11f869b5c5444f
Vamsimothalla <42707756+Vamsimothalla@users.noreply.github.com>
2020-06-21 09:27:33 +0530

    Update warns.py

--- haruka/modules/warns.py
+++ haruka/modules/warns.py
@@ -29,7 +29,7 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
     if is_user_admin(chat, user.id):
         message.reply_text("I'm not going to warn an admin!")
         return ""
-    elif is_user_res(chat,user.id):
+    if is_user_res(chat,user.id):
         message.reply_text("I'm not going to warn an admin!")
         return ""
 
